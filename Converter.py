3ac7f513ea65c9fe9d712325b12e93050aa7a46b
miro <miro@electronic.works>
2016-03-07 21:14:31 +0100

    Cleaned files and added a gnu comment

--- Converter.py
+++ Converter.py
@@ -1,5 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 # -*- coding: UTF-8 -*-
+
+#* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+#*                                                                         *
+#*  Copyright (C) 2016 Linux statt Windows                                 *
+#*                                                                         *
+#*  'vanilla-nodebb-importer' is free software:                            *
+#*  you can redistribute it and/or modify                                  *
+#*  it under the terms of the GNU General Public License as published by   *
+#*  the Free Software Foundation, version 3 of the License.                *
+#*                                                                         *
+#*  'vanilla-nodebb-importer' is distributed in the hope that it will be   *
+#*  useful, but WITHOUT ANY WARRANTY; without even the implied warranty    *
+#*  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       *
+#*  GNU General Public License for more details.                           *
+#*                                                                         *
+#*  You should have received a copy of the GNU General Public License      *
+#*  along with 'vanilla-nodebb-importer'. If not, see                      *
+#*  <http://www.gnu.org/licenses/>.                                        *
+#*                                                                         *
+#* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 '''
 @description
